5be95a50376a5ce90c22da4cb3be46d9d150fa33
kgitbank <kgitbank@DESKTOP-U6AB831>
2018-09-19 09:10:18 +0900

    ddd

--- src/controllers/JoinController.java
+++ src/controllers/JoinController.java
@@ -1,8 +1,11 @@
 package controllers;
 
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 
-@WebServlet("join.do")
-public class JoinController {
+@WebServlet("/join.do")
+public class JoinController extends HttpServlet{
+	
+	
 
 }
